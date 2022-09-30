3baae92876fc7fcc5ac923ba0340896cc4ba9899
Julia Ch <juliecha@yandex.ru>
2022-09-30 21:17:20 +0300

    UPD

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -32,3 +32,33 @@ else
 {
     Console.WriteLine("Такого дня недели нет )))");
 }
+
+// Console.WriteLine("Введите день недели Например 1 -> Понедельник");
+//         int a = Convert.ToInt32(Console.ReadLine());
+//         switch (a)
+//         {
+//             case 1:
+//                 Console.WriteLine("Понедельник");
+//                 break;
+//             case 2:
+//                 Console.WriteLine("Вторник");
+//                 break;
+//             case 3:
+//                 Console.WriteLine("Среда");
+//                 break;
+//             case 4:
+//                 Console.WriteLine("Четверг");
+//                 break;
+//             case 5:
+//                 Console.WriteLine("Пятница");
+//                 break;
+//             case 6:
+//                 Console.WriteLine("Суббота");
+//                 break;
+//             case 7:
+//                 Console.WriteLine("Воскресенье");
+//                 break;
+//                 default:
+//                 Console.WriteLine("Вы ввели не коректное число");
+//                 break;
+//         }
