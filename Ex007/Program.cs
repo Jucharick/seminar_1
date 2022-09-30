977d4f25b7469a484741a1bbbf990eeb983895c1
Julia Ch <juliecha@yandex.ru>
2022-09-30 21:47:29 +0300

    Add Ex007 and UPD Ex005

---
+++ Ex007/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 100 | n > 999)
+{
+    Console.WriteLine("Введите целое трехзначное число");
+}
+else 
+{
+    Console.Write("Последнее число = " + Convert.ToInt32(n%10));
+}
