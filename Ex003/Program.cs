48e6d257564245102a0dd786b9ab0f5e019aebcd
Julia Ch <juliecha@yandex.ru>
2022-09-30 21:06:58 +0300

    Add Ex003

---
+++ Ex003/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите день недели Например 1 -> Monday");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 1)
+{
+    Console.WriteLine("Monday");
+}
+if (n == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+if (n == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+if (n == 4)
+{
+    Console.WriteLine("Thursday");
+}
+if (n == 5)
+{
+    Console.WriteLine("Friday");
+}
+if (n == 6)
+{
+    Console.WriteLine("Saturday");
+}
+if (n == 7)
+{
+    Console.WriteLine("Sunday");
+}
+if (n > 7)
+{
+    Console.WriteLine("Такого дня недели нет )))");
+}
