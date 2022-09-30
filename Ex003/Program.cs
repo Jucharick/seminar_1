426e791417d8d3b9b53a425b705c888afab13d4c
Julia Ch <juliecha@yandex.ru>
2022-09-30 21:13:53 +0300

    UPD Ex003

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -4,31 +4,31 @@ if (n == 1)
 {
     Console.WriteLine("Monday");
 }
-if (n == 2)
+else if (n == 2)
 {
     Console.WriteLine("Tuesday");
 }
-if (n == 3)
+else if (n == 3)
 {
     Console.WriteLine("Wednesday");
 }
-if (n == 4)
+else if (n == 4)
 {
     Console.WriteLine("Thursday");
 }
-if (n == 5)
+else if (n == 5)
 {
     Console.WriteLine("Friday");
 }
-if (n == 6)
+else if (n == 6)
 {
     Console.WriteLine("Saturday");
 }
-if (n == 7)
+else if (n == 7)
 {
     Console.WriteLine("Sunday");
 }
-if (n > 7)
+else  
 {
     Console.WriteLine("Такого дня недели нет )))");
 }
