6d71d6d95f15eb1ebacbb1a78ec714505dd8b7dc
Julia Ch <juliecha@yandex.ru>
2022-09-30 20:52:42 +0300

    Add Ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите еще одно целое число");
+int m = Convert.ToInt32(Console.ReadLine());
+if (m*m==n)
+{
+    Console.WriteLine("Yes");
+}
+else 
+{
+    Console.WriteLine("No");
+}
